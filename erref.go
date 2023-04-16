f225f04936680abc3e8b82585767be3acfc5c1ff
Pichu <pichu.baby@gmail.com>
2023-04-16 23:30:36 +0800

    update login part
    Signed-off-by: Pichu <pichu.baby@gmail.com>

--- erref.go
+++ erref.go
@@ -2,4 +2,5 @@ package simba
 
 const (
 	STATUS_MORE_PROCESSING_REQUIRED uint32 = 0xC0000016
+	STATUS_LOGON_FAILURE            uint32 = 0xC000006D
 )
--- server.go
+++ server.go
@@ -64,11 +64,11 @@ func (c *conn) serve() {
 		case SMB2_NEGOTIATE:
 			fmt.Printf("SMB2_NEGOTIATE\n")
 			msg := NegotiateRequest(r[64:])
-			c.handleNegotiate(r.SessionId(), msg)
+			c.handleNegotiate(r, msg)
 		case SMB2_SESSION_SETUP:
 			fmt.Printf("SMB2_SESSION_SETUP\n")
 			msg := SessionSetupRequest(r[64:])
-			c.handleSessionSetup(r.SessionId(), msg)
+			c.handleSessionSetup(r, msg)
 
 		default:
 			fmt.Printf("unknown command: %v (%d)\n", r.Command(), r.Command())
@@ -112,7 +112,7 @@ func (c *conn) readRequest() (w PacketCodec, err error) {
 
 }
 
-func (c *conn) handleNegotiate(sessionID uint64, msg NegotiateRequest) error {
+func (c *conn) handleNegotiate(p PacketCodec, msg NegotiateRequest) error {
 	fmt.Printf("handleNegotiate: %v\n", msg.ClientGuid())
 
 	securityBufferPayload := auth.DefaultNegoPayload
@@ -163,9 +163,9 @@ func (c *conn) handleNegotiate(sessionID uint64, msg NegotiateRequest) error {
 	smb2Header.SetCreditRequestResponse(1)
 	smb2Header.SetFlags(SMB2_FLAGS_SERVER_TO_REDIR)
 	smb2Header.SetNextCommand(0)
-	smb2Header.SetMessageId(0)
+	smb2Header.SetMessageId(p.MessageId())
 	smb2Header.SetTreeId(0)
-	smb2Header.SetSessionId(sessionID)
+	smb2Header.SetSessionId(p.SessionId())
 	smb2Header.SetSignature([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
 
 	l := len(smb2Header) + len(responseHdr)
@@ -188,8 +188,55 @@ func (c *conn) handleNegotiate(sessionID uint64, msg NegotiateRequest) error {
 
 }
 
-func (c *conn) handleSessionSetup(sessionID uint64, msg SessionSetupRequest) error {
+func (c *conn) handleSessionSetup(p PacketCodec, msg SessionSetupRequest) error {
 	fmt.Printf("handleSessionSetup: %v\n", msg)
+
+	// get NTLMSSP message
+	gssBuffer := msg.Buffer()
+	var mechToken []byte
+	if gssBuffer[0] == 0x60 {
+		gssPayload, err := auth.NewInitPayload(gssBuffer)
+		if err != nil {
+			log.Printf("handleSessionSetup NewInitPayload: %v", err)
+			return fmt.Errorf("handleSessionSetup NewInitPayload: %v", err)
+		}
+		mechToken = gssPayload.Token.NegTokenInit.MechToken
+	} else if gssBuffer[0] == 0xa1 {
+		gssPayload, err := auth.NewTargPayload(gssBuffer)
+		if err != nil {
+			log.Printf("handleSessionSetup NewTargPayload: %v", err)
+			return fmt.Errorf("handleSessionSetup NewTargPayload: %v", err)
+		}
+		mechToken = gssPayload.ResponseToken
+
+	}
+
+	// get NTLMSSP message
+	fmt.Printf("mechToken: %v\n", hex.EncodeToString(mechToken))
+
+	ntlmsspPayload := auth.NTLMMessage(mechToken)
+	if ntlmsspPayload.IsInvalid() {
+		return fmt.Errorf("handleSessionSetup NTLMMessage is invalid")
+	}
+
+	switch ntlmsspPayload.MessageType() {
+	case auth.NTLMSSP_NEGOTIATE:
+		log.Printf("NTLM_NEGOTIATE: %v\n", len(ntlmsspPayload))
+		return c.handleSessionSetupNtmlsspNetotiate(p, msg, auth.NTLMNegotiateMessage(mechToken))
+	case auth.NTLMSSP_AUTH:
+		log.Printf("NTLMSSP_AUTH: %v\n", len(ntlmsspPayload))
+		return c.handleSessionSetupNtmlsspAuth(p, msg, auth.NTLMNegotiateMessage(mechToken))
+	default:
+		fmt.Printf("NTLMSSP unknown message type: %0x\n", ntlmsspPayload.MessageType())
+		// case auth.NTLM_CHALLENGE:
+		// 	fmt.Printf("NTLM_CHALLENGE: %v\n", ntlmsspPayload)
+		// case auth.NTLM_AUTHENTICATE:
+		// 	fmt.Printf("NTLM_AUTHENTICATE: %v\n", ntlmsspPayload)
+	}
+	return fmt.Errorf("unknown ntlm message type: %0x\n", ntlmsspPayload.MessageType())
+}
+func (c *conn) handleSessionSetupNtmlsspNetotiate(p PacketCodec, msg SessionSetupRequest, ntlpPayload auth.NTLMNegotiateMessage) error {
+
 	pkt := []byte{}
 	securityBuffer, _ := hex.DecodeString("a181c43081c1a0030a0101a10c060a2b06010401823702020aa281ab0481a84e544c4d5353500002000000140014003800000015828ae2b5bdb4abf704918f00000000000000005c005c004c000000060100000000000f4d00420056004d00320032003100320030003800020014004d00420056004d00320032003100320030003800010014004d00420056004d0032003200310032003000380004000000030014006d00620076006d003200320031003200300038000700080060bbda0f486dd90100000000")
 	log.Printf("securityBuffer lenght: %v", len(securityBuffer))
@@ -209,14 +256,61 @@ func (c *conn) handleSessionSetup(sessionID uint64, msg SessionSetupRequest) err
 	smb2Header.SetCreditRequestResponse(1)
 	smb2Header.SetFlags(SMB2_FLAGS_SERVER_TO_REDIR)
 	smb2Header.SetNextCommand(0)
-	smb2Header.SetMessageId(1)
+	smb2Header.SetMessageId(p.MessageId())
 	smb2Header.SetTreeId(0)
 	// if sessionID == 0 {
 	// 	sessionID = 0xebc20a15
 	// } else {
 	// 	sessionID++
 	// }
-	smb2Header.SetSessionId(sessionID)
+	smb2Header.SetSessionId(p.SessionId())
+	smb2Header.SetSignature([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+
+	l := len(smb2Header) + len(responseHdr)
+	log.Printf("smb2 length: %v, resp len: %v\n", len(smb2Header), len(responseHdr))
+	netBIOSHeader := []byte{0x00, 0x00, 0x00, 0x00}
+	netBIOSHeader[3] = byte(l)
+	netBIOSHeader[2] = byte(l >> 8)
+
+	// smb2Header := []byte{0xfe, 0x53, 0x4d, 0x42, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+
+	pkt = append(pkt, netBIOSHeader...)
+	pkt = append(pkt, smb2Header...)
+	pkt = append(pkt, responseHdr...)
+
+	fmt.Printf("handleSessionSetup: %v\n", hex.EncodeToString(pkt))
+	c.rwc.Write(pkt)
+	fmt.Printf("send response: %d\n", len(pkt))
+
+	return nil
+}
+
+func (c *conn) handleSessionSetupNtmlsspAuth(p PacketCodec, msg SessionSetupRequest, ntlpPayload auth.NTLMNegotiateMessage) error {
+
+	pkt := []byte{}
+	responseHdr := SessionSetupResponse(make([]byte, 8))
+	responseHdr.SetStructureSize()
+	responseHdr.SetSecurityBufferOffset(0)
+	responseHdr.SetSecurityBufferLength(0)
+	// responseHdr.SetSecurityMode(Securi)
+
+	smb2Header := PacketCodec(make([]byte, 64))
+	smb2Header.SetProtocolId()
+	smb2Header.SetStructureSize()
+	smb2Header.SetCreditCharge(1)
+	smb2Header.SetCommand(SMB2_SESSION_SETUP)
+	smb2Header.SetStatus(STATUS_LOGON_FAILURE)
+	smb2Header.SetCreditRequestResponse(1)
+	smb2Header.SetFlags(SMB2_FLAGS_SERVER_TO_REDIR)
+	smb2Header.SetNextCommand(0)
+	smb2Header.SetMessageId(p.MessageId())
+	smb2Header.SetTreeId(0)
+	// if sessionID == 0 {
+	// 	sessionID = 0xebc20a15
+	// } else {
+	// 	sessionID++
+	// }
+	smb2Header.SetSessionId(p.SessionId())
 	smb2Header.SetSignature([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
 
 	l := len(smb2Header) + len(responseHdr)
